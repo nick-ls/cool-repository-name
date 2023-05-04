c7c8ea1806bf97f8f8ad135abba3c5d85ec35cc0
Nick P <22565232+nick-ls@users.noreply.github.com>
2023-05-04 08:45:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Set the target date and time (in UTC)
 const targetDate = new Date(Date.UTC(2023, 4, 6, 0, 0, 0)); // May 6, 2023 at 00:00:00 UTC
 
-// Get the HTML elements for the countdown timer
-const cdElem = document.getElementById("cd");
-const chElem = document.getElementById("ch");
-const cmElem = document.getElementById("cm");
-const csElem = document.getElementById("cs");
-
 // Update the countdown timer every second
 setInterval(() => {
+  // Get the HTML elements for the countdown timer
+  const cdElem = document.getElementById("cd");
+  const chElem = document.getElementById("ch");
+  const cmElem = document.getElementById("cm");
+  const csElem = document.getElementById("cs");
+  
   // Get the current date and time (in UTC)
   const now = new Date();
 
